238e8dc882e122ce47064a9aa9bcd58f7fe5dae8
komal kalra <komalkalra698@gmail.com>
2025-08-27 19:23:58 +0530

    Here's my first NodeJS file

---
+++ LAB-DAY1/Function.js
@@ -0,0 +1,9 @@
+var name = "Komal kalra";
+var age = 21;
+var isname = true;
+
+function Myfunction(name, age, isname){
+    return(
+        console.log(`welcome, hii is this your name-  ${name}, here's your age- ${age}, and is this you ${isname} `)
+    )
+} 
